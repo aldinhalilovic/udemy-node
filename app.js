7662ce36245237e6090feba1ecf0028d3724d8f8
aldinhalilovic <aldinhalilovic06@gmail.com>
2023-07-15 18:27:08 +0200

    node udemy

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>Hello from ALdin</h1>");
+});
+
+const server = http.createServer(app);
+
+server.listen(4000);
